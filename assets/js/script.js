ff8dbc180f4725336c6293e3e0607bf2176ae4ae
Stephen Moore <stephenmoore33@outlook.com>
2021-05-11 11:29:34 -0400

    added script to save search history to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
 
+    let savedInput = JSON.parse(localStorage.getItem("savedInput"));
+    if (savedInput === null) {
+        savedInput = ["Detroit"]
+    }
 
 
+$(".citySearch").on("click", function () {
+
+
+
+}
     
 });
